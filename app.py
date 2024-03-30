50ed070883f9d94d64656097b30c782ed26f1573
prabhaulakh10 <156348073+prabhaulakh10@users.noreply.github.com>
2024-03-29 23:19:10 -0400

    Tabular display for hum,pressure,temp,weather

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import requests
+import json
 import streamlit as st
 
 def get_current_weather(city):
     api_key = "c42a09c1a3e8685b4d0cbfda38c22023"
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+    
     try:
         response = requests.get(url)
         response.raise_for_status()  # Raise an exception for any HTTP errors
@@ -19,19 +21,32 @@ def get_current_weather(city):
     except json.JSONDecodeError as err:
         st.error(f"Error: Failed to parse response JSON - {err}")
         return None
-        
-    # Extract relevant current weather information
+    
+    # Extract relevant weather information
     temperature = data['main']['temp']
+    humidity = data['main']['humidity']
+    weather_desc = data['weather'][0]['description']
+    pressure = data['main']['pressure']
     
     # Convert temperature from Kelvin to Celsius
     temperature = round(temperature, 2)
     
-    return temperature
+    return temperature, humidity, weather_desc, pressure
 
+# Streamlit UI
 st.markdown("<h1 style='color: white; font-family: Copperplate Gothic Bold;'>🌤️ Weather Forecasting Application</h1>", unsafe_allow_html=True)
 
 city = st.text_input("Enter city name")
 if st.button("Get Weather"):
-    current_temperature = get_current_weather(city)
-    if current_temperature:
-        st.write(f"Temperature: {current_temperature}°C")
+    weather_data = get_current_weather(city)
+    if weather_data:
+        temperature, humidity, weather_desc, pressure = weather_data
+        
+        # Display weather information in tabular form
+        weather_table = {
+            'Temperature (°C)': [temperature],
+            'Humidity (%)': [humidity],
+            'Weather': [weather_desc],
+            'Pressure (hPa)': [pressure]
+        }
+        st.table(weather_table)
